e440df6f12586149faf25fe685e341665d7ef083
Nathanael-Lontsi <tchoupounathanael@gmail.com>
2023-06-06 13:00:42 +0100

    done some changes

---
+++ src/modal/Dummydata.tsx
@@ -0,0 +1,70 @@
+export const Posts = [
+  {
+    id: 1,
+    photo: "https://images.pexels.com/photos/17047402/pexels-photo-17047402/free-photo-of-la-gravedad-del-lugar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    date: "1 min ago",
+    userId: 1,
+    like: 1,
+    comment: 1,
+  },
+  {
+    id: 2,
+    photo: "https://images.pexels.com/photos/17047402/pexels-photo-17047402/free-photo-of-la-gravedad-del-lugar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    date: "2 min ago",
+    userId: 2,
+    like: 2,
+    comment: 2,
+  },
+  {
+    id: 3,
+    photo: "https://images.pexels.com/photos/17047402/pexels-photo-17047402/free-photo-of-la-gravedad-del-lugar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    date: "3 min ago",
+    userId: 3,
+    like: 3,
+    comment: 3,
+  },
+  {
+    id: 4,
+    photo: "https://images.pexels.com/photos/17047402/pexels-photo-17047402/free-photo-of-la-gravedad-del-lugar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    date: "4 min ago",
+    userId: 4,
+    like: 4,
+    comment: 4,
+  },
+  {
+    id: 5,
+    photo: "https://images.pexels.com/photos/17047402/pexels-photo-17047402/free-photo-of-la-gravedad-del-lugar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    date: "5 min ago",
+    userId: 5,
+    like: 5,
+    comment: 5,
+  },
+];
+
+export const Users = [
+  {
+    id: 1,
+    profile: "https://images.pexels.com/photos/17018371/pexels-photo-17018371/free-photo-of-sea-landscape-sunset-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    username: "Ryan Sim",
+  },
+  {
+    id: 1,
+    profile: "https://images.pexels.com/photos/17018371/pexels-photo-17018371/free-photo-of-sea-landscape-sunset-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    username: "Billar Gill",
+  },
+  {
+    id: 2,
+    profile: "https://images.pexels.com/photos/17018371/pexels-photo-17018371/free-photo-of-sea-landscape-sunset-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    username: "Jenn Arkansas",
+  },
+  {
+    id: 1,
+    profile: "https://images.pexels.com/photos/17018371/pexels-photo-17018371/free-photo-of-sea-landscape-sunset-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    username: "Layeur Brook",
+  },
+  {
+    id: 1,
+    profile: "https://images.pexels.com/photos/17018371/pexels-photo-17018371/free-photo-of-sea-landscape-sunset-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    username: "Ashana Ducher",
+  },
+]
